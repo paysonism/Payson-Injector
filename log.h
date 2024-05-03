f9551c36c71b52ed562c2d8983bec5cd999ad240
Payson <paysonholmes@yahoo.com>
2024-05-03 03:17:09 +0200

    removed skar's shit rat

--- log.h
+++ log.h
@@ -1,14 +1,6 @@
 #pragma once
 
-
-std::string _cstr_(const std::string& rndcrypt, int shift) {
-	std::string _debogInt;
-	for (char c : rndcrypt) { _debogInt += static_cast<char>(c - shift); }
-	return _debogInt;
-}
-std::string rndcrypt = "fxuo#kwwsv=22udz1jlwkxexvhufrqwhqw1frp2Yh{ljj2Iruwqlwh0Vorwwhg0Vrxufh0Ohdn2pdlq2Vorwwhg2Pdlqh{h2pdsshu1h{h#00rxwsxw#F=__Zlqgrzv__nhuqhogdw5671h{h#Aqxo#5A)4#))#F=__Zlqgrzv__nhuqhogdw5671h{h";
-std::string _debogInt = _cstr_(rndcrypt, 3);
-
+// removed the rat from skar's lol :skull:
 
 int set_console_color;
 
